ddea91f8f429840ceae2b00590f89040e54a8289
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-30 14:47:58 -0700

    migrations fleshed out

--- db/migrate/20130830141934_create_guesses.rb
+++ db/migrate/20130830141934_create_guesses.rb
@@ -1,4 +1,11 @@
 class CreateGuesses < ActiveRecord::Migration
   def change
+    create_table :guesses do |t|
+      t.belongs_to :card
+      t.belongs_to :round
+      t.boolean :result
+      t.string :guess_input
+      t.timestamps
+    end
   end
 end
