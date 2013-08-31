cc0ed5165c0215e32808f569c57575418c64ff20
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-31 15:32:43 -0700

    partials integrated into card views, 2 routs refactored into 1

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -56,44 +56,43 @@ post '/deck/:id' do
   round = Round.create(deck_id: params[:id], user_id: session[:user_id])
   @deck = Deck.find(round.deck_id)
   session[:cards] = shuffle_deck(@deck) 
-  redirect "/round/#{round.id}"
+  redirect "/round/#{round.id}/"
 end
 
 
-
-
-get '/round/:id' do
+get '/round/:id/:guess_id?' do
   @round = Round.find(params[:id])
   @deck = Deck.find(@round.deck_id)
+
+  if params[:guess_id]
+    @guess = Guess.find(params[:guess_id])
+    if @guess.result == true
+      @result = erb :result_page_correct, :layout => false
+    else
+      @result = erb :result_page_incorrect, :layout => false
+    end
+  end
+
   if session[:cards].empty?
     erb :finished_round
   else
     @current_card = Card.find(set_current_card)
     erb :play_card
   end
+
 end
+
 
 post '/card/:id/:round_id' do
   @current_card = Card.find(params[:id])
   @round = Round.find(params[:round_id])
   @guess = Guess.new(round_id: @round.id, guess_input: params["guess_input"]) 
-  if @current_card.answer == @guess.guess_input
+  if @current_card.answer.downcase == @guess.guess_input.downcase
     @guess.result = true
   else
     @guess.result = false
   end
   
   @guess.save
-  redirect "/round/guess/#{@guess.id}/#{@current_card.id}"
-end
-
-get '/round/guess/:guess_id/:card_id' do
-  @guess = Guess.find(params[:guess_id])
-  @round = Round.find(@guess.round_id)
-  @current_card = Card.find(params[:card_id])
-  if @guess.result == true
-    erb :result_page_correct
-  else
-    erb :result_page_incorrect
-  end
+  redirect "/round/#{@round.id}/#{@guess.id}"
 end
