a631140e41f9fae5dbff2d38a5987bbd11678557
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-31 18:55:26 -0700

    minor seed file fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ b = Deck.create(title: "Cohorts")
 
 Card.create(question: "What is the capitol of California?", answer: "Sacramento", deck_id: a.id)
 Card.create(question: "What is the capitol of Washington?", answer: "Olympia", deck_id: a.id)
-Card.create(question: "What is the capitol of Nobody Cares?", answer: "Nobody Cares", deck_id: a.id)
+Card.create(question: "What is the capitol of Hawaii?", answer: "Honolulu", deck_id: a.id)
 Card.create(question: "What is the capitol of Nebraska?", answer: "Lincoln", deck_id: a.id)
 Card.create(question: "Which cohort is named after a bird?", answer: "Purple Martins", deck_id: b.id)
 Card.create(question: "Which cohort has the coolest members?", answer: "Mule Deer", deck_id: b.id)
