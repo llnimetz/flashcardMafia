6abe09ce4fe11e2ceeec82313cbd38c409874231
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-31 17:58:30 -0700

    Updated migration to include card foreign key

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,5 +1,6 @@
 class Card < ActiveRecord::Base
   # Remember to create a migration!
   has_one :deck
+  has_many :guesses
 
 end
--- db/migrate/20130830141934_create_guesses.rb
+++ db/migrate/20130830141934_create_guesses.rb
@@ -2,6 +2,7 @@ class CreateGuesses < ActiveRecord::Migration
   def change
     create_table :guesses do |t|
       t.belongs_to :round
+      t.belongs_to :card
       t.boolean :result
       t.string :guess_input
       t.timestamps
